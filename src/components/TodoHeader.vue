3e9ceb0b395089620ce4e241729092ffd27c4666
ostapenkovitalij <vitost666@gmail.com>
2025-08-15 20:34:29 +0300

    feat: add AddButton, TodoHeader, and TodoList components

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SearchBar from './SearchBar.vue'
+import TodoFilter from './TodoFilter.vue'
+</script>
+
+<template>
+  <h1 class="text-2xl font-bold mb-8">TODO LIST</h1>
+
+  <div class="flex space-x-2 mb-6 w-full max-w-xl">
+    <search-bar class="flex-grow" />
+    <todo-filter />
+  </div>
+</template>
+
+<style scoped></style>
