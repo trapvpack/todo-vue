4b12a5e196a3ed93be75547f7b8619e83a239ff5
ostapenkovitalij <vitost666@gmail.com>
2025-08-15 20:33:09 +0300

    fix: cfg tailwind fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
