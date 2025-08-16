8827e97528d7641896c823912c7cfbc49ccee44d
ostapenkovitalij <vitost666@gmail.com>
2025-08-16 16:16:30 +0300

    feat: implemented filter btn

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
 import SearchBar from './SearchBar.vue'
 import TodoFilter from './TodoFilter.vue'
+import { ref, computed } from 'vue'
+const filters = ref([
+  { id: 1, text: 'All', isActive: true },
+  { id: 2, text: 'Completed', isActive: false },
+  { id: 3, text: 'Incompleted', isActive: false },
+])
+
+const props = defineProps<{
+  activeFilter: number
+}>()
+
+const emit = defineEmits<{
+  (e: 'filter', selectedFilter: { id: number; text: string; isActive: boolean }): void
+}>()
+
+function onFilterSelected(selectedFilter: { id: number; text: string; isActive: boolean }) {
+  filters.value.forEach((f) => {
+    f.isActive = f.id === selectedFilter.id
+  })
+  emit('filter', selectedFilter)
+}
 </script>
 
 <template>
   <h1 class="text-2xl font-bold mb-8">TODO LIST</h1>
 
-  <div class="flex space-x-2 mb-6 w-full max-w-xl">
+  <div class="flex justify-start space-x-2 w-full max-w-xl mb-6 items-center">
     <search-bar class="flex-grow" />
-    <todo-filter />
+    <todo-filter :filters="filters" @filter="onFilterSelected" />
   </div>
 </template>
 
