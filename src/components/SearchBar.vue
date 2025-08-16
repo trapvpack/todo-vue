e5cda9eb66fb56ad069ca32cb3850cfd950ddb53
ostapenkovitalij <vitost666@gmail.com>
2025-08-16 16:15:31 +0300

    feat: formating changes

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,28 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import searchIcon from "../assets/icons/Vector.svg"
-const searchQuery = ref('');
+import { ref } from 'vue'
+import searchIcon from '../assets/icons/Vector.svg'
+const searchQuery = ref('')
 </script>
 
 <template>
-    <div class="relative flex items-center mx-4">
-    <input class="flex-grow mx-4 px-3 py-2 
-    border rounded-md ring-indigo-400 
-    focus:outline-none focus:border-none focus:ring-2 focus:ring-indigo-500"
-    placeholder="Search notes"
-    type="text"
-    v-model="searchQuery"
-    @keyup.enter="$emit('search', searchQuery)"
-    @keyup.esc="$emit('search', '')"
-    aria-label="Search notes"       
-    ></input>
+  <div class="relative flex items-center">
+    <input
+      class="flex-grow px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+      placeholder="Search notes"
+      type="text"
+      v-model="searchQuery"
+      @keyup.enter="$emit('search', searchQuery)"
+      @keyup.esc="$emit('search', '')"
+      aria-label="Search notes"
+    />
     <img
       class="w-5 h-5 absolute right-7 pointer-events-none"
       :src="searchIcon"
       alt="search icon"
       aria-hidden="true"
     />
-</div>
+  </div>
 </template>
 
 <style scoped></style>
