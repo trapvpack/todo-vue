92ec227f1e9ae00b8b7e92e10cb715804c45adf7
ostapenkovitalij <vitost666@gmail.com>
2025-08-20 01:35:53 +0300

    feat: add closing on outside click

--- src/components/TodoFilter.vue
+++ src/components/TodoFilter.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, useTemplateRef, watch } from 'vue'
+import { onClickOutside } from '@vueuse/core'
 const props = defineProps<{
   filters: Array<{ id: number; text: string }>
   activeFilter: number
@@ -23,13 +24,27 @@ const activeFilterText = computed(() => {
 function isActive(filterId: number) {
   return props.activeFilter === filterId
 }
+
+const target = useTemplateRef<HTMLElement>('target')
+
+watch(target, (el) => {
+  if (el) {
+    onClickOutside(
+      el,
+      () => {
+        open.value = false
+      },
+      { capture: true, event: 'pointerdown' },
+    )
+  }
+})
 </script>
 
 <template>
-  <div class="relative inline-block text-left">
+  <div class="relative inline-block text-left" ref="target">
     <button
       class="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 w-30 cursor-pointer transition-colors duration-300"
-      @click="toggleDropdown"
+      @click.stop="toggleDropdown"
     >
       {{ activeFilterText }}
     </button>
@@ -42,7 +57,7 @@ function isActive(filterId: number) {
           class="px-4 py-2 hover:bg-gray-100 cursor-pointer"
           :class="{ 'bg-indigo-100': isActive(filter.id) }"
         >
-          <button class="w-full text-left cursor-pointer" @click="selectFilter(filter.id)">
+          <button class="w-full text-left cursor-pointer" @click.stop="selectFilter(filter.id)">
             {{ filter.text }}
           </button>
         </li>
